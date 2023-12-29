7235f6ec76dc2e448a7723c02a2db9593c31d3aa
Marius Topircean <mtopircean@yahoo.com>
2023-12-29 19:29:50 +0000

    Creating first stateless function

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,18 @@
+import React from "react";
+/* not needed in latest react versions */
+
+// function FunctionalGreeting() {
+//     return <h1>Hello from React!</h1>
+// }
+
+const FunctionalGreeting = () => <h1>Hello from React!</h1>;
+
+// const FunctionalGreeting: This line declares a constant variable named FunctionalGreeting. In JavaScript, const is used to declare a variable that cannot be reassigned after it's initially set. In this case, it's assigned an arrow function.
+
+// () =>: This is the arrow function syntax in JavaScript. It's a concise way to define a function. In this case, it's an anonymous arrow function that takes no arguments because the parentheses () are empty.
+
+// <h1>Hello from React!</h1>: This is JSX (JavaScript XML) syntax used in React to define a piece of user interface. It represents an HTML <h1> element with the text "Hello from React!" inside it.
+
+// ;: This is a semicolon, which is used to terminate the statement. In JavaScript, you can often omit semicolons at the end of lines, but they are considered a good practice to avoid potential issues
+
+export default FunctionalGreeting;
