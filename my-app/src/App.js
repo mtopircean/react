eb89cbb85d01f0ff07fc0f44386e3e729a4108bc
Marius Topircean <mtopircean@yahoo.com>
2023-12-30 17:43:40 +0000

    Completed the statefull part of the class

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefullGreeting from './components/StatefullGreeting';
+import StatefullGreetingWithCallback from './components/StatefullGreetingWithCalback';
+import StatefullGreetingWithPrevState from './components/StatefullGreetingWithPrevState';
 
 function App() {
   return (
     <div className="App">
+      <StatefullGreetingWithPrevState greeting="I`m a statefull class component!" name="I am Loki!"/>
+      <StatefullGreetingWithCallback greeting="I`m a statefull class component!" name="I am Loki!"/>
+      <StatefullGreeting greeting="I`m a statefull class component!" name="I am Loki!"/>
       <FunctionalGreeting />
       <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Mike" age="32" />
     </div>
