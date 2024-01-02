b1ea59c0e3e8db2132d1ed88135bbb9feac81861
Marius Topircean <mtopircean@yahoo.com>
2024-01-02 13:34:01 +0000

    CControlled and Uncontrolled forms

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -18,10 +18,14 @@ import Clock from './components/Clock';
 import LifeCyclesCDM from './components/LifeCyclesCDM';
 import LifeCyclesCDU from './components/LifeCyclesCDU';
 import LifeCyclesCWU from "./components/LifeCyclesCWU";
+import ControlledForm from './components/ControlledForm';
+import UncontrolledForm from './components/UncontrolledForm';
 
 function App() {
   return (
     <div className="App">
+      <UncontrolledForm />
+      <ControlledForm />
       <LifeCyclesCWU />
       <LifeCyclesCDU />
       <LifeCyclesCDM />
