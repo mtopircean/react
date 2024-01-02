fbc8f909ec53b7049a366a9bf206707cd19b8b16
Marius Topircean <mtopircean@yahoo.com>
2024-01-02 14:10:06 +0000

    Filter searchbar challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -20,10 +20,12 @@ import LifeCyclesCDU from './components/LifeCyclesCDU';
 import LifeCyclesCWU from "./components/LifeCyclesCWU";
 import ControlledForm from './components/ControlledForm';
 import UncontrolledForm from './components/UncontrolledForm';
+import { SearchBar } from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
+      <SearchBar />
       <UncontrolledForm />
       <ControlledForm />
       <LifeCyclesCWU />
